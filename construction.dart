4f35f1c084cec2f5d82ca48d4e93cf992ee9cde1
VISHNU G S <72614129+vishnu852@users.noreply.github.com>
2021-06-01 23:40:17 +0530

    Add files via upload

---
+++ construction.dart
@@ -0,0 +1,21 @@
+import '/D:/flutter pgms/src/pac.dart';
+
+class Desktop
+{
+	void acer()
+	{
+		print("acer rated 5 star for all models");
+	}
+
+}
+
+
+
+void main()
+{
+	
+	Desktop da=new Desktop();
+	da.acer();
+	Laptop la=new Laptop();
+	la.asus();
+}
